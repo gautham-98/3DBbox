7f81b3e13a210cac8765f40a7de1988772505d2f
gautham-98 <gauthammohan1998@gmail.com>
2026-04-12 22:52:20 +0200

    loss lamdas in dict

--- src/training/losses.py
+++ src/training/losses.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from .rot_utils import rot6d_to_rotmat
+from typing import TypedDict
 
 def loss_tr(pred_tr, gt_tr):
     return F.smooth_l1_loss(pred_tr, gt_tr)
@@ -8,12 +8,9 @@ def loss_tr(pred_tr, gt_tr):
 def loss_lwh(pred_delta_lwh, gt_delta_lwh):
     return F.smooth_l1_loss(pred_delta_lwh, gt_delta_lwh)
 
-def loss_rot(pred_6d, gt_R):
-    """geodesic rotation loss"""
-    pred_R = rot6d_to_rotmat(pred_6d)
-    
+def loss_rot(pred_rot, gt_rot):
     # get error
-    err_R = pred_R.transpose(-2,-1) @ gt_R #(B, 3, 3)
+    err_R = pred_rot.transpose(-2,-1) @ gt_rot #(B, 3, 3)
     trace = torch.diagonal(err_R, dim1=-2, dim2=-1).sum(-1) #(B,)
 
     # find cos angle and clamp 
@@ -23,3 +20,10 @@ def loss_rot(pred_6d, gt_R):
 
 def loss_corners(pred_corners, gt_corners):
     return (pred_corners - gt_corners).norm(dim=-1).sum(dim=-1).mean()
+
+class LossLambda(TypedDict):
+    lwh: float
+    rot: float
+    tr: float
+    corner: float
+
