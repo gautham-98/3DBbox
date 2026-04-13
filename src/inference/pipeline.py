ee557126c533ca795f9defe2b6a9fe01f603ee87
gautham-98 <gauthammohan1998@gmail.com>
2026-04-13 16:43:18 +0200

    visualise proposal boxes

--- src/inference/pipeline.py
+++ src/inference/pipeline.py
@@ -9,7 +9,7 @@ from src.data.preprocess import (
     create_o3d_pcd,
 )
 from src.models.boxestimator import BoxEstimationNet
-from src.utils.box_utils import reconstruct_bbox
+from src.utils.box_utils import reconstruct_bbox, BBOX3D_CORNERS
 from src.utils.rot_utils import rot6d_to_rotmat
 
 
@@ -27,9 +27,11 @@ class BoxPredictor:
         self.num_points = num_points
         self.frame = canonical_frame
         self.batch_sz = batch_sz
+
         self._pc_full: np.ndarray = None
         self._rgb_full: np.ndarray = None
-        self._result:np.ndarray = None
+        self._result: np.ndarray = None
+        self._proposal: np.ndarray = None
 
     def __call__(self, pc_path: str, rgb_path: str, mask_path: str) -> np.ndarray:
         """returns an array of bbox corners in world frame"""
@@ -37,6 +39,7 @@ class BoxPredictor:
             pc_path, rgb_path, mask_path
         )
         self._result = []
+        self._proposal = []
         dataloader = self.preprocess(self._pc_full, self._rgb_full, masks)
 
         for batch in tqdm(dataloader, desc="processing"):
@@ -54,16 +57,27 @@ class BoxPredictor:
             pred_rot = rot6d_to_rotmat(pred_6d)
             pred_lwh = batch["prop_lwh"] * (1 + delta_lwh)
 
+            bboxes_proposal: torch.Tensor = (
+                BBOX3D_CORNERS[None, ...]
+                * batch["prop_lwh"][:, None, :]
+                @ batch["rotation"].transpose(-1, -2)
+                + batch["translation"][:, None, :]
+            )
+
             bboxes_canonical = reconstruct_bbox(pred_lwh, pred_rot, pred_tr)
             bboxes_world: torch.Tensor = (
                 bboxes_canonical @ batch["rotation"].transpose(-1, -2)
                 + batch["translation"][:, None, :]
             )
+
             self._result.extend([bbox.detach().cpu().numpy() for bbox in bboxes_world])
+            self._proposal.extend(
+                [bbox.detach().cpu().numpy() for bbox in bboxes_proposal]
+            )
 
         self._result = np.array(self._result)
         return self._result
-    
+
     def preprocess(self, pc_full, rgb_full, masks):
         """return batched instances"""
 
@@ -75,23 +89,24 @@ class BoxPredictor:
             processed_dict = preprocess_pointcloud(
                 pts=pts_i, cols=cols_i, N=self.num_points, canonical_frame=self.frame
             )
-            points.append(processed_dict["points"])
-            colors.append(processed_dict["cols"])
-            rotation.append(processed_dict["rotation"])
-            translation.append(processed_dict["translation"])
-            lwh.append(processed_dict["lwh"])
+            if processed_dict is not None:
+                points.append(processed_dict["points"])
+                colors.append(processed_dict["cols"])
+                rotation.append(processed_dict["rotation"])
+                translation.append(processed_dict["translation"])
+                lwh.append(processed_dict["lwh"])
 
-            if (idx + 1) % self.batch_sz == 0:
-                dataloader.append(
-                    dict(
-                        points=torch.stack(points, dim=0),
-                        colors=torch.stack(colors, dim=0),
-                        rotation=torch.stack(rotation, dim=0),
-                        translation=torch.stack(translation, dim=0),
-                        prop_lwh=torch.stack(lwh, dim=0),
+                if len(points) == self.batch_sz:
+                    dataloader.append(
+                        dict(
+                            points=torch.stack(points, dim=0),
+                            colors=torch.stack(colors, dim=0),
+                            rotation=torch.stack(rotation, dim=0),
+                            translation=torch.stack(translation, dim=0),
+                            prop_lwh=torch.stack(lwh, dim=0),
+                        )
                     )
-                )
-                points, colors, rotation, translation, lwh = [], [], [], [], []
+                    points, colors, rotation, translation, lwh = [], [], [], [], []
         # add the rest as final data to dataloader
         if points:
             dataloader.append(
@@ -116,8 +131,8 @@ class BoxPredictor:
 
         valid = self._pc_full[2] > 0.01  # (H, W) bool
 
-        points = self._pc_full[:, valid].T   # (N, 3)
-        colors = self._rgb_full[valid] / 255.0       # (N, 3)
+        points = self._pc_full[:, valid].T  # (N, 3)
+        colors = self._rgb_full[valid] / 255.0  # (N, 3)
 
         pcd = create_o3d_pcd(pts=points, cols=colors)
 
@@ -144,6 +159,14 @@ class BoxPredictor:
             box.paint_uniform_color([0.0, 1.0, 0.0])
             pred_boxes.append(box)
 
+        prop_boxes = []
+        for corners in self._proposal:
+            box = o3d.geometry.LineSet()
+            box.points = o3d.utility.Vector3dVector(corners)
+            box.lines = o3d.utility.Vector2iVector(edges)
+            box.paint_uniform_color([1.0, 0.0, 0.0])
+            prop_boxes.append(box)
+
         if gt_corners is not None:
             gt_boxes = []
             for corners in gt_corners:
@@ -153,24 +176,31 @@ class BoxPredictor:
                 box.paint_uniform_color([0.0, 0.0, 1.0])
                 gt_boxes.append(box)
 
-        geoms = [pcd] + pred_boxes
+        geoms = [pcd] + pred_boxes + prop_boxes
         if gt_corners is not None:
             geoms += gt_boxes
 
-        o3d.visualization.draw_geometries(geoms)
+        o3d.visualization.draw_geometries(
+            geoms,
+            window_name="BBox Prediction - Green, Prop - Red"
+            + (", GT - Blue" if gt_corners is not None else ""),
+        )
 
 
 if __name__ == "__main__":
     import os
+    from src.data.splits import get_splits
+
     os.environ["XDG_SESSION_TYPE"] = "x11"
 
-    SAMPLE = "8b061a8a-9915-11ee-9103-bbb8eae05561"
+    split_paths = get_splits("dataset")
+    SAMPLE = split_paths["test"][4]
 
-    PC_PATH = f"dataset/{SAMPLE}/pc.npy"
-    RGB_PATH = f"dataset/{SAMPLE}/rgb.jpg"
-    MASK_PATH = f"dataset/{SAMPLE}/mask.npy"
-    BOX_PATH = f"dataset/{SAMPLE}/bbox3d.npy"
-    CHECKPOINT = "checkpoints/checkpoint_epoch95.pth"
+    PC_PATH = f"{SAMPLE}/pc.npy"
+    RGB_PATH = f"{SAMPLE}/rgb.jpg"
+    MASK_PATH = f"{SAMPLE}/mask.npy"
+    BOX_PATH = f"{SAMPLE}/bbox3d.npy"
+    CHECKPOINT = "checkpoints/checkpoint_epoch_best.pth"
     IN_CHANNELS = 6
     DEVICE = "cpu"
 
