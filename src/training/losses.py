40d6e50df28d2330210ea566c39a0f2ed9634094
gautham-98 <gauthammohan1998@gmail.com>
2026-04-14 08:49:58 +0000

    losses made more robust through focal methods

--- src/training/losses.py
+++ src/training/losses.py
@@ -5,18 +5,22 @@ from typing import TypedDict
 def loss_tr(pred_tr, gt_tr):
     return F.smooth_l1_loss(pred_tr, gt_tr)
 
-def loss_lwh(pred_delta_lwh, gt_delta_lwh):
-    return F.smooth_l1_loss(pred_delta_lwh, gt_delta_lwh)
+def loss_lwh(pred_delta_lwh, gt_delta_lwh, beta=0.1):
+    return F.smooth_l1_loss(pred_delta_lwh, gt_delta_lwh, beta=beta)
 
-def loss_rot(pred_rot, gt_rot):
+def loss_rot(pred_rot, gt_rot, gamma=1.5):
     # get error
     err_R = pred_rot.transpose(-2,-1) @ gt_rot #(B, 3, 3)
     trace = torch.diagonal(err_R, dim1=-2, dim2=-1).sum(-1) #(B,)
 
     # find cos angle and clamp 
     cos_angle = ((trace - 1) / 2).clamp(-1 + 1e-6, 1 - 1e-6)
+    angle = torch.acos(cos_angle)
     
-    return torch.acos(cos_angle).mean() # TODO: check if maps to 0-3.14
+    # find cosine score per element 0.5*(1-cos_angle) maps (0,180) -> (0,1)
+    weight = (0.5*(1-cos_angle.detach())) ** gamma
+
+    return (angle * weight).mean()
 
 def loss_corners(pred_corners, gt_corners):
     return (pred_corners - gt_corners).norm(dim=-1).sum(dim=-1).mean()
